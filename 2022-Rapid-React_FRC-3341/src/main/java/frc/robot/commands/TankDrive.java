f619ae2a88961f58c50d43213540250c07ba0341
AnirudhKannan1 <90531142+AnirudhKannan1@users.noreply.github.com>
2022-01-22 15:21:26 -0800

    Update TankDrive.java

--- 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/commands/TankDrive.java
+++ 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/commands/TankDrive.java
@@ -4,23 +4,25 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.DriveTrain;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.Joystick;
+import frc.robot.Constants;
+  
 
 /** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
-
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+public class TankDrive extends CommandBase {
+  /** Creates a new TankDrive. */
+  private final DriveTrain _driveTrain;
+  private final Joystick _leftJoystick;
+  private final Joystick _rightJoystick;
+  public TankDrive(DriveTrain dt, Joystick leftJ, Joystick rightJ) {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+    _driveTrain = dt;
+    _leftJoystick = leftJ;
+    _rightJoystick = rightJ;
+
+    addRequirements(_driveTrain);
   }
 
   // Called when the command is initially scheduled.
@@ -29,7 +31,11 @@ public class ExampleCommand extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    System.out.println("TankDrive");
+    _driveTrain.tankDrive(-0.8 * _leftJoystick.getRawAxis(Constants.JoystickAxis.YAxis),
+                          -0.8 * _rightJoystick.getRawAxis(Constants.JoystickAxis.YAxis)); 
+  }
 
   // Called once the command ends or is interrupted.
   @Override
