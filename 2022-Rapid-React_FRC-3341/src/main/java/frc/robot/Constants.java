9b451635f13ae2abc35720244b235fbcac083c36
AnirudhKannan1 <90531142+AnirudhKannan1@users.noreply.github.com>
2022-01-22 15:21:47 -0800

    Update Constants.java

--- 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/Constants.java
+++ 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/Constants.java
@@ -12,4 +12,17 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants 
+{
+    public static final class DriveTrainPorts
+    {
+        public static final int LeftDriveTalonPort = 2; //need to check and test later
+        public static final int RightDriveTalonPort = 3;
+    }
+
+    public static final class JoystickAxis 
+    {
+        public static final int YAxis = 1;
+        public static final int XAxis = 0;
+    }
+}
