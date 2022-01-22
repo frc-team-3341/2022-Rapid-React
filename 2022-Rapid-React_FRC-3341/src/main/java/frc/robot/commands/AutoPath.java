cd88a01c098ac4f727e5556494451538ad33dcf7
AnirudhKannan1 <90531142+AnirudhKannan1@users.noreply.github.com>
2022-01-22 15:21:09 -0800

    Update AutoPath.java

--- 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/commands/AutoPath.java
+++ 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/commands/AutoPath.java
@@ -5,39 +5,14 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 /** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
-
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+public class AutoPath extends SequentialCommandGroup 
+{
+    public AutoPath()
+    {
+        addCommands(new AutoDriveForward(RobotContainer.getDriveTrain(), 2.3));
+    }
 }
