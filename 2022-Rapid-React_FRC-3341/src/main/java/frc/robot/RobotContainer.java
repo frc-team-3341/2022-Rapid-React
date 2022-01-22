8cc935e8494688b3caa581aeda4e6ad29f51135f
AnirudhKannan1 <90531142+AnirudhKannan1@users.noreply.github.com>
2022-01-22 15:22:05 -0800

    Update RobotContainer.java

--- 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/RobotContainer.java
+++ 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,12 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.ArcadeDrive;
+import frc.robot.commands.TankDrive;
+import frc.robot.commands.AutoPath;
+import frc.robot.commands.AutoDriveForward;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -21,6 +26,13 @@ public class RobotContainer {
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+  private static DriveTrain _DriveTrain;
+
+  private static AutoDriveForward driveForward;
+
+  private static AutoPath autoPath;
+
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -43,6 +55,10 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return autoPath;
+  }
+
+  public static DriveTrain getDriveTrain(){
+    return _DriveTrain;
   }
 }
