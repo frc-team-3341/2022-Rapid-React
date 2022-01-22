4cdd37934d0131fa3f1b9b749bfc23b5c3d71cd1
AnirudhKannan1 <90531142+AnirudhKannan1@users.noreply.github.com>
2022-01-22 15:17:53 -0800

    Update DriveTrain.java

--- 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,19 +4,79 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-public class ExampleSubsystem extends SubsystemBase {
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+
+import com.kauailabs.navx.frc.AHRS;
+
+public class DriveTrain extends SubsystemBase 
+{
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  private final WPI_TalonSRX _leftDriveTalon;
+  private final WPI_TalonSRX _rightDriveTalon;
+  private AHRS navx = new AHRS(SPI.Port.kMXP);
+  private double ticksToCm  = 80.0/10180.5; //will test constant later
+  private final int ticksInOneRevolution = 4096; 
+ 
+  private DifferentialDrive _diffDrive;
+
+  public DriveTrain() 
+  {
+    _leftDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.LeftDriveTalonPort);
+    _rightDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.RightDriveTalonPort);
+
+    _leftDriveTalon.setInverted(true);
+    _rightDriveTalon.setInverted(false);
+
+    _diffDrive = new DifferentialDrive(_leftDriveTalon, _rightDriveTalon);
+
+    _leftDriveTalon.configFactoryDefault();
+    _leftDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+    _rightDriveTalon.configFactoryDefault();
+
+
+    _rightDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+
+  }
+
+  public void tankDrive(double leftSpeed, double rightSpeed) {
+    _diffDrive.tankDrive(leftSpeed, rightSpeed);
+  }
+
+  public void arcadeDrive(double speed, double turn) {
+    _diffDrive.arcadeDrive(speed, turn);
+  }
+
+  public void resetEncoders() {
+    _leftDriveTalon.setSelectedSensorPosition(0,0,10);
+    _rightDriveTalon.setSelectedSensorPosition(0,0,10);
+  }
+
+  public double getPosition() {
+    return ((_leftDriveTalon.getSelectedSensorPosition() + _rightDriveTalon.getSelectedSensorPosition())/2) * (ticksToCm);
+    //average distance of both left and right
+  }
+
+  public double getTicks() {
+    return (_leftDriveTalon.getSelectedSensorPosition(0) + _rightDriveTalon.getSelectedSensorPosition(0)) / 2;
+  }
 
   @Override
-  public void periodic() {
+  public void periodic() 
+  {
     // This method will be called once per scheduler run
   }
 
   @Override
-  public void simulationPeriodic() {
+  public void simulationPeriodic() 
+  {
     // This method will be called once per scheduler run during simulation
   }
 }
