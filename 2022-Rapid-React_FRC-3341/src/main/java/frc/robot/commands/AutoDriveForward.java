ac73c6fdb067970aca28deda5a5f38c09bfc76ce
facethechallenge69 <54116672+facethechallenge69@users.noreply.github.com>
2022-01-22 16:00:32 -0800

    1/22 final

--- 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/commands/AutoDriveForward.java
+++ 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/commands/AutoDriveForward.java
@@ -6,38 +6,68 @@ package frc.robot.commands;
 
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.DriveTrain;
 
 /** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+public class AutoDriveForward extends CommandBase 
+{
+  private final DriveTrain _DriveTrain;
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+  private double distance;
+  private double speed;
+  private double error;
+  private double kP = 0.8; //test constant later
+
+  public AutoDriveForward(DriveTrain dt, double dist) 
+  {
+    distance = dist;
+    _DriveTrain = dt;
+    addRequirements(_DriveTrain);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() 
+  {
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() 
+  {
+    SmartDashboard.putNumber("Ticks", _DriveTrain.getTicks());
+    error = distance - _DriveTrain.getPosition();
+    error = (error / distance)*2;
+    speed = error * kP;
+
+    if(speed > .7)
+    {
+      speed = .7;
+    }
+
+    if(speed < .2)
+    {
+      speed = .2;
+    }
+
+    SmartDashboard.putNumber("Current Speed", speed);
+    SmartDashboard.putNumber("Current Distance", _DriveTrain.getPosition());
+    _DriveTrain.tankDrive(speed,speed);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) 
+  {
+    _DriveTrain.tankDrive(0, 0);
+  }
 
   // Returns true when the command should end.
   @Override
-  public boolean isFinished() {
-    return false;
+  public boolean isFinished() 
+  {
+    return _DriveTrain.getPosition() >= distance;
   }
 }
