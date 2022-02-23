344e5c9a00f9127a84de1d01276e32dd3cfd4e97
AnirudhKannan1 <90531142+AnirudhKannan1@users.noreply.github.com>
2022-02-22 19:45:54 -0800

    Update RobotContainer.java

--- 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/RobotContainer.java
+++ 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/RobotContainer.java
@@ -5,15 +5,17 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ArcadeDrive;
 import frc.robot.commands.TankDrive;
 import frc.robot.commands.AutoPath;
 import frc.robot.commands.AutoDriveForward;
+import frc.robot.commands.TurnGyroPID;
 import frc.robot.subsystems.DriveTrain;
 import edu.wpi.first.wpilibj2.command.Command;
 
-import frc.robot.subsystems.MaxbotixUltrasonicSensor;
+//import frc.robot.subsystems.MaxbotixUltrasonicSensor;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -24,15 +26,29 @@ import frc.robot.subsystems.MaxbotixUltrasonicSensor;
 public class RobotContainer {
 
   private static DriveTrain _DriveTrain;
+  
+  private final Joystick _leftJoystick;
+  private final Joystick _rightJoystick;
 
   private static AutoDriveForward driveForward;
 
+  private final TankDrive _tankDrive;
+  private final ArcadeDrive _arcadeDrive;
+
   private static AutoPath autoPath;
   
-  MaxbotixUltrasonicSensor ultrasonicSensor = new MaxbotixUltrasonicSensor(Constants.I2CAddresses.MaxbotixUltrasonicSensor);
+  //MaxbotixUltrasonicSensor ultrasonicSensor = new MaxbotixUltrasonicSensor(Constants.I2CAddresses.MaxbotixUltrasonicSensor);
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
+    _leftJoystick = new Joystick(Constants.USBOrder.Two);
+    _rightJoystick = new Joystick(Constants.USBOrder.Three);
+    _DriveTrain = new DriveTrain();
+    _tankDrive = new TankDrive(_DriveTrain, _leftJoystick, _rightJoystick);
+    _DriveTrain.setDefaultCommand(_tankDrive);
+    _arcadeDrive = new ArcadeDrive(_DriveTrain, _leftJoystick);
+    driveForward = new AutoDriveForward(_DriveTrain, -100);
+    autoPath = new AutoPath();
     configureButtonBindings();
   }
 
