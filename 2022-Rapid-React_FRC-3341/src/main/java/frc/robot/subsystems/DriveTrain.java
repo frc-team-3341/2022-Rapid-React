46770a58e1ac03a218ab20a0400e0fd0cf1d7b74
AnirudhKannan1 <90531142+AnirudhKannan1@users.noreply.github.com>
2022-02-22 19:46:09 -0800

    Update DriveTrain.java

--- 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -6,10 +6,12 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.Victor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
@@ -21,21 +23,30 @@ public class DriveTrain extends SubsystemBase
   /** Creates a new ExampleSubsystem. */
   private final WPI_TalonSRX _leftDriveTalon;
   private final WPI_TalonSRX _rightDriveTalon;
+  private final VictorSPX _leftDriveVictor;
+  private final VictorSPX _rightDriveVictor;
+
   private AHRS navx = new AHRS(SPI.Port.kMXP);
-  private double ticksToCm  = 80.0/10180.5; //will test constant later
+  private double ticksToCm  = 100.0/12774; //will test constant later
   private final int ticksInOneRevolution = 4096; 
  
-  private DifferentialDrive _diffDrive;
 
   public DriveTrain() 
   {
     _leftDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.LeftDriveTalonPort);
     _rightDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.RightDriveTalonPort);
+    _leftDriveVictor = new VictorSPX(Constants.DriveTrainPorts.LeftDriveVictorPort);
+    _rightDriveVictor = new VictorSPX(Constants.DriveTrainPorts.RightDriveVictorPort);
+
+    _leftDriveVictor.follow(_leftDriveTalon);
+    _rightDriveVictor.follow(_rightDriveTalon);
 
     _leftDriveTalon.setInverted(true);
     _rightDriveTalon.setInverted(false);
+    _leftDriveVictor.setInverted(InvertType.FollowMaster);
+    _rightDriveVictor.setInverted(InvertType.FollowMaster);
 
-    _diffDrive = new DifferentialDrive(_leftDriveTalon, _rightDriveTalon);
+
 
     _leftDriveTalon.configFactoryDefault();
     _leftDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
@@ -47,11 +58,11 @@ public class DriveTrain extends SubsystemBase
   }
 
   public void tankDrive(double leftSpeed, double rightSpeed) {
-    _diffDrive.tankDrive(leftSpeed, rightSpeed);
-  }
+    _leftDriveTalon.set(ControlMode.PercentOutput, -leftSpeed);
+    _rightDriveTalon.set(ControlMode.PercentOutput, -rightSpeed);  }
 
   public void arcadeDrive(double speed, double turn) {
-    _diffDrive.arcadeDrive(speed, turn);
+    //_diffDrive.arcadeDrive(speed, turn);
   }
 
   public void resetEncoders() {
@@ -68,10 +79,26 @@ public class DriveTrain extends SubsystemBase
     return (_leftDriveTalon.getSelectedSensorPosition(0) + _rightDriveTalon.getSelectedSensorPosition(0)) / 2;
   }
 
+  public double getAngleAndReset(){
+    double degrees = navx.getAngle();
+    navx.reset();
+    return degrees;
+  }
+ 
+  public double getAngle(){
+    return navx.getAngle(); 
+  }
+ 
+  public void resetN(){
+    navx.reset();
+  }
+
   @Override
   public void periodic() 
   {
     // This method will be called once per scheduler run
+    //arcadeDrive(0.8 * RobotContainer.getLeftJoy().getRawAxis(Constants.JoystickAxis.XAxis),
+    //0.8 * RobotContainer.getLeftJoy().getRawAxis(Constants.JoystickAxis.YAxis));    System.out.println("PERIODIC RUNNING");
   }
 
   @Override
